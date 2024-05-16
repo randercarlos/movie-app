4fa69f5e1eeb11dcebb322c539a2dbdb806019fe
Rander <rfreitas@uplexis.com.br>
2024-05-16 16:56:52 -0300

    feat: add template components

---
+++ src/components/template/TheFooter.vue
@@ -0,0 +1,11 @@
+<template>
+  <footer class="border border-t border-gray-800">
+    <div class="container mx-auto text-sm px-4 py-6">
+      Powered by <a
+        href="https://www.themoviedb.org/documentation/api"
+        class="underline hover:text-gray-300"
+        target="_blank"
+      >TMDb API</a>
+    </div>
+  </footer>
+</template>
---
+++ src/components/template/TheHeader.vue
@@ -0,0 +1,9 @@
+<template>
+  <header class="border-b border-gray-800">
+    <TheNavigation />
+  </header>
+</template>
+
+<script setup lang='ts'>
+import TheNavigation from "@/components/template/TheNavigation.vue";
+</script>
