8d0b5bf33ac25c40cc530787881a6fc901e118f0
Rander <rfreitas@uplexis.com.br>
2024-05-17 08:47:05 -0300

    feat: add Movies pages and components

---
+++ src/components/Movie/MovieList.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-8">
+    <!-- <MovieListItem v-for="movie in popularMovies" :key="movie.id" :movie="movie" /> -->
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
