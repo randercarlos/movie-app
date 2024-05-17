5dd59decdd412c711e5a5e39f9b9f1c57ac99674
Rander <rfreitas@uplexis.com.br>
2024-05-17 14:42:00 -0300

    feat: add Movie page

--- src/pages/MoviesPage.vue
+++ src/pages/MoviesPage.vue
@@ -4,28 +4,25 @@
       <h2 class="uppercase tracking-wider text-orange-500 text-lg font-semibold">
         Popular Movies
       </h2>
-      <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-8">
-        @foreach ($popularMovies as $movie)
-        <x-movie-card movie="$movie" />
-        @endforeach
-      </div>
+      <MovieList :movies="popularMovies" />
     </div> <!-- end pouplar-movies -->
 
     <div class="now-playing-movies py-24">
       <h2 class="uppercase tracking-wider text-orange-500 text-lg font-semibold">
         Now Playing
       </h2>
-      <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-8">
-        @foreach ($nowPlayingMovies as $movie)
-        <x-movie-card movie="$movie" />
-        @endforeach
-      </div>
+      <MovieList :movies="nowPlayingMovies" />
     </div> <!-- end now-playing-movies -->
   </div>
 </template>
 
 <script setup lang="ts">
+import { ref } from "vue";
+import { Movie } from "@/models/interfaces";
+import MovieList from "@/components/Movie/MovieList.vue";
 
+const popularMovies = ref<Array<Movie>>([]);
+const nowPlayingMovies = ref<Array<Movie>>([]);
 </script>
 
 <style scoped>
