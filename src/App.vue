271c233f30f5721bc9b79205f77c4a9c43e3204e
Rander <rfreitas@uplexis.com.br>
2024-05-21 19:03:13 -0300

    feat: update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,17 @@
 <template>
   <TheHeader />
-  <RouterView />
+  <RouterView v-slot="{ Component }">
+    <template v-if="Component">
+      <Suspense>
+        <template #default>
+          <component :is="Component" />
+        </template>
+        <template #fallback>
+          Loading...
+        </template>
+      </Suspense>
+    </template>
+  </RouterView>
   <TheFooter />
 </template>
 
