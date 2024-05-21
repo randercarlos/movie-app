5b964dcb5a34fc19f2e31163219775ea0337381e
Rander <rfreitas@uplexis.com.br>
2024-05-21 18:59:44 -0300

    chore: app and typescript configs

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,11 @@
-const Config = {
-  apiBaseUrl: String(import.meta.env.VITE_API_HOST) || "",
+const CONFIG = {
+  APP_NAME: "MovieApp",
+  APP_BASE_URL: String(import.meta.env.VITE_BASE_URL) || "",
+  API_BASE_URL: String(import.meta.env.VITE_API_HOST) || "",
+  TMDB_TOKEN: String(import.meta.env.VITE_TMDB_TOKEN) || "",
+  MOVIE_CREW_QTY: 3,
+  MOVIE_CAST_QTY: 5,
+  MOVIE_IMAGES_QTY: 12,
 };
 
-export default Config;
+export default CONFIG;
