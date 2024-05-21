1fefe5fbc56dae05a351a487de356e88a2aaeb65
Rander <rfreitas@uplexis.com.br>
2024-05-21 19:01:50 -0300

    feat: add movie components

--- src/components/Movie/MovieList.vue
+++ src/components/Movie/MovieList.vue
@@ -1,13 +1,16 @@
 <template>
-  <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-8">
-    <!-- <MovieListItem v-for="movie in popularMovies" :key="movie.id" :movie="movie" /> -->
+  <div class="grid grid-cols-1 gap-8 lg:grid-cols-5 md:grid-cols-3 sm:grid-cols-2">
+    <MovieListItem
+      v-for="movie in props.movies"
+      :key="movie.id"
+      :movie="movie"
+    />
   </div>
 </template>
 
 <script setup lang="ts">
+import MovieListItem from "@/components/movie/MovieListItem.vue";
+import type { MoviesListProps } from "@/typings/props";
 
+const props = defineProps<MoviesListProps>();
 </script>
-
-<style scoped>
-
-</style>
--- src/components/Movie/MovieListItem.vue
+++ src/components/Movie/MovieListItem.vue
@@ -1,33 +1,42 @@
 <template>
-  <div class="container mx-auto px-4 pt-16">
-    <div class="popular-movies">
-      <h2 class="uppercase tracking-wider text-orange-500 text-lg font-semibold">
-        Popular Movies
-      </h2>
-      <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-8">
-        @foreach ($popularMovies as $movie)
-        <x-movie-card movie="$movie" />
-        @endforeach
+  <div class="mt-8">
+    <RouterLink :to="{ name: 'movie', params: { movieId: props.movie?.id} }">
+      <img
+        :src="`https://image.tmdb.org/t/p/w500/${props.movie?.poster_path}`"
+        alt="poster"
+        class="transition duration-150 ease-in-out hover:opacity-75"
+      >
+    </RouterLink>
+    <div class="mt-2">
+      <RouterLink
+        :to="{ name: 'movie', params: { movieId: props.movie?.id} }"
+        class="mt-2 text-lg hover:text-gray-300"
+      >
+        {{ props.movie?.title }}
+      </RouterLink>
+      <div class="mt-1 flex items-center text-sm text-gray-400">
+        <svg
+          class="w-4 fill-current text-orange-500"
+          viewBox="0 0 24 24"
+        ><g data-name="Layer 2"><path
+          d="M17.56 21a1 1 0 01-.46-.11L12 18.22l-5.1 2.67a1 1 0 01-1.45-1.06l1-5.63-4.12-4a1 1 0
+          01-.25-1 1 1 0 01.81-.68l5.7-.83 2.51-5.13a1 1 0 011.8 0l2.54 5.12 5.7.83a1 1 0 01.81.68
+          1 1 0 01-.25 1l-4.12 4 1 5.63a1 1 0 01-.4 1 1 1 0 01-.62.18z"
+          data-name="star"
+        /></g></svg>
+        <span class="ml-1">{{ props.movie?.vote_average }}</span>
+        <span class="mx-2">|</span>
+        <span>{{ props.movie?.release_date }}</span>
       </div>
-    </div> <!-- end pouplar-movies -->
-
-    <div class="now-playing-movies py-24">
-      <h2 class="uppercase tracking-wider text-orange-500 text-lg font-semibold">
-        Now Playing
-      </h2>
-      <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-8">
-        @foreach ($nowPlayingMovies as $movie)
-        <x-movie-card movie="$movie" />
-        @endforeach
+      <div class="text-sm text-gray-400">
+        {{ props.movie?.genres }}
       </div>
-    </div> <!-- end now-playing-movies -->
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import type { MoviesListItemProps } from "@/typings/props";
 
+const props = defineProps<MoviesListItemProps>();
 </script>
-
-<style scoped>
-
-</style>
