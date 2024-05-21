a9f3bc5213c54a5409c3c9a418cefc381b2de492
Rander <rfreitas@uplexis.com.br>
2024-05-21 19:04:30 -0300

    feat: add routes

--- src/router.ts
+++ src/router.ts
@@ -1,16 +1,48 @@
 import { createRouter, createWebHistory } from "vue-router";
+import Movies from "@/pages/Movies.vue";
 
-const MoviesPage = () => import("@/pages/MoviesPage.vue");
-
+const Movie = () => import("@/pages/Movie.vue");
 const router = createRouter({
   history: createWebHistory(import.meta.env.VITE_BASE_URL),
   routes: [
     {
       path: "/",
-      name: "moviesPage",
-      component: MoviesPage
+      name: "home",
+      redirect: { name: "movies" }
     },
-  ]
+    {
+      path: "/movies",
+      name: "movies",
+      component: Movies
+    },
+    {
+      path: "/movies/:movieId(\\d+)",
+      name: "movie",
+      component: Movie
+    },
+    {
+      path: "/tv-shows",
+      name: "tv-shows",
+      component: Movies
+    },
+    {
+      path: "/tv-shows/:tvshowId(\\d+)",
+      name: "tv-show",
+      component: Movies
+    },
+    {
+      path: "/actors",
+      name: "actors",
+      component: Movies
+    },
+    {
+      path: "/actors/:actorId(\\d+)",
+      name: "actor",
+      component: Movies
+    },
+    // { path: "/:pathMatch(.*)*", name: "NotFound", component: NotFound },
+  ],
+  strict: true
 });
 
 export default router;
