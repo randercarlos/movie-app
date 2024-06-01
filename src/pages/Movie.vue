dfa5770573d11af663cb234da6bfc9f5e0052f36
Rander <rfreitas@uplexis.com.br>
2024-06-01 09:47:33 -0300

    refactor: move business rules and api requests to composables

--- src/pages/Movie.vue
+++ src/pages/Movie.vue
@@ -3,56 +3,20 @@
 </template>
 
 <script setup lang="ts">
-import { onMounted, reactive, ref } from "vue";
+import { ref, type MaybeRef } from "vue";
 import MovieListItemDetails from "@/components/movie/MovieListItemDetails.vue";
 import { useRoute } from "vue-router";
-import { useApiService } from "@/composables/useApiService";
+import { useMovieDetails } from "@/composables/useMovieDetails";
 import type { MovieDetails, MovieDetailsResponse } from "@/typings/interfaces";
-import collect from "collect.js";
-import moment from "moment";
-import CONFIG from "@/config";
+import { useMovieDetailsModelView } from "@/composables/useMovieDetailsModelView";
 
 const route = useRoute();
 const movieId = ref<number>(+route.params.movieId);
-
 const movieDetails = ref<MovieDetails>();
-const movieDetailEndpoint = ref<string>("movie");
 
-async function getMovieDetails(): Promise<void> {
-  const apiService = reactive(useApiService(movieDetailEndpoint));
-  const params = ref<Record<string, string>>({
-    "append_to_response": "credits,videos,images"
-  });
-  const { data } = await apiService.find<MovieDetailsResponse>(movieId, params);
-
-  movieDetails.value = mountMovieModelView(data.value as MovieDetailsResponse);
-}
-
-function mountMovieModelView(movie: MovieDetailsResponse): MovieDetails {
-  return collect(movie)
-    .merge({
-      "poster_path": movie.poster_path
-        ? `https://image.tmdb.org/t/p/w500/${movie.poster_path}`
-        : "https://via.placeholder.com/500x750",
-      "vote_average": `${movie.vote_average * 10}%`,
-      "release_date": moment(movie.release_date).format("MMM DD, YYYY"),
-      "genres": collect(movie.genres).pluck("name").flatten().implode(", "),
-      "crew": collect(movie.credits?.crew).take(CONFIG.MOVIE_CREW_QTY).all(),
-      "cast": collect(movie.credits?.cast).take(CONFIG.MOVIE_CAST_QTY).map(function(cast) {
-        return collect(cast).merge({
-          "profile_path": cast.profile_path
-            ? `https://image.tmdb.org/t/p/w300${cast.profile_path}`
-            : "https://via.placeholder.com/300x450",
-        }).all();
-      }).all(),
-      "images": collect(movie.images?.backdrops)?.take(CONFIG.MOVIE_IMAGES_QTY).all(),
-    })
-    .only(["poster_path", "id", "genres", "title", "vote_average", "overview", "release_date",
-      "credits", "videos", "images", "crew", "cast", "images"])
-    .all() as unknown as MovieDetails;
-}
-
-onMounted(async() => {
-  await getMovieDetails();
-});
+const { data: movieDetailsResponse } = await useMovieDetails(movieId);
+const { data: movieDetailsModelView } = useMovieDetailsModelView(
+  movieDetailsResponse as MaybeRef<MovieDetailsResponse>,
+);
+movieDetails.value = movieDetailsModelView.value;
 </script>
--- src/pages/Movies.vue
+++ src/pages/Movies.vue
@@ -17,84 +17,30 @@
 </template>
 
 <script setup lang="ts">
-import { onMounted, reactive, ref, shallowRef } from "vue";
-import type {
-  Movie,
-  MovieGenre,
-  MovieGenreResponse,
-  MovieResponse,
-  MovieResponseResult
-} from "@/typings/interfaces";
+import { useMovieGenres } from "@/composables/useMovieGenres";
 import MovieList from "@/components/movie/MovieList.vue";
-import { useApiService } from "@/composables/useApiService";
-import collect from "collect.js";
-import { formatDate, formatNumber } from "@/utils/helper";
+import { useMoviesModelView } from "@/composables/useMoviesModelView";
+import { useNowPlayingMovies } from "@/composables/useNowPlayingMovies";
+import { usePopularMovies } from "@/composables/usePopularMovies";
+import type { Movie, MovieGenresResponse, MovieResponse } from "@/typings/interfaces";
+import { shallowRef, type MaybeRef } from "vue";
 
-const moviesGenres = shallowRef<Array<MovieGenre>>([]);
-const popularMovies = shallowRef<Array<Movie>>([]);
-const nowPlayingMovies = shallowRef<Array<Movie>>([]);
+const popularMovies = shallowRef<Movie[]>();
+const nowPlayingMovies = shallowRef<Movie[]>([]);
 
-const moviesGenresEndpoint = ref<string>("genre/movie/list");
-const popularMoviesEndpoint = ref<string>("movie/popular");
-const nowPlayingMoviesEndpoint = ref<string>("movie/now_playing");
+const { data: moviesGenresResponse } = await useMovieGenres();
+const { data: popularMoviesResponse } = await usePopularMovies();
+const { data: nowPlayingMoviesResponse } = await useNowPlayingMovies();
 
-async function getPopularMovies(): Promise<MovieResponse> {
-  const apiService = reactive(useApiService(popularMoviesEndpoint));
-  const { data } = await apiService.list<MovieResponse>();
+const { data: popularMoviesModelView } = useMoviesModelView(
+  popularMoviesResponse as MaybeRef<MovieResponse>,
+  moviesGenresResponse as MaybeRef<MovieGenresResponse>
+);
+popularMovies.value = popularMoviesModelView.value;
 
-  return data.value as MovieResponse;
-}
-
-async function getNowPlayingMovies(): Promise<MovieResponse> {
-  const apiService = reactive(useApiService(nowPlayingMoviesEndpoint));
-  const { data } = await apiService.list<MovieResponse>();
-
-  return data.value as MovieResponse;
-}
-
-async function getMoviesGenres(): Promise<MovieGenre[]> {
-  const apiService = reactive(useApiService(moviesGenresEndpoint));
-  const { data } = await apiService.list<MovieGenreResponse>();
-
-  return (data.value as MovieGenreResponse).genres;
-}
-
-function mountMoviesModelView(movieResponse: MovieResponse): Movie[] {
-  return collect(movieResponse.results)
-    .map(function(movieResponseResult: MovieResponseResult) {
-      const genresFormatted = collect(movieResponseResult.genre_ids)
-        .mapWithKeys((genreId: number) => [genreId, getGenreNameById(genreId)])
-        .values()
-        .implode(", ");
-
-      return collect(movieResponseResult)
-        .merge({
-          "poster_path":  `https://image.tmdb.org/t/p/w500/${movieResponseResult.poster_path}`,
-          "vote_average": `${formatNumber(movieResponseResult.vote_average * 10, 0, 2)}%`,
-          "release_date": formatDate(movieResponseResult.release_date, "MMM DD, YYYY"),
-          "genres": genresFormatted,
-        })
-        .only([
-          "poster_path", "id", "genre_ids", "title", "vote_average", "overview", "release_date",
-          "genres",
-        ])
-        .all() as unknown as Movie;
-    }).all() as unknown as Movie[];
-}
-
-function getGenreNameById(genreId: number): string {
-  return collect(moviesGenres.value)
-    .mapWithKeys((genre: MovieGenre) => [genre.id, genre.name])
-    .get(genreId) as unknown as string;
-}
-
-onMounted(async() => {
-  moviesGenres.value = await getMoviesGenres();
-  popularMovies.value = mountMoviesModelView(await getPopularMovies());
-  nowPlayingMovies.value = mountMoviesModelView(await getNowPlayingMovies());
-});
+const { data: nowPlayingMoviesModelView } = useMoviesModelView(
+  nowPlayingMoviesResponse as MaybeRef<MovieResponse>,
+  moviesGenresResponse as MaybeRef<MovieGenresResponse>
+);
+nowPlayingMovies.value = nowPlayingMoviesModelView.value;
 </script>
-
-<style scoped>
-
-</style>
