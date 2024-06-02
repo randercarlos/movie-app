7db3e2ee6dc9bd6743f072b8419840b3eb24ae26
Rander <rfreitas@uplexis.com.br>
2024-06-02 10:05:11 -0300

    feat: add notivue components

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,30 @@ import "@unocss/reset/tailwind.css";
 import "virtual:uno.css";
 import "virtual:unocss-devtools";
 
+import "notivue/notification.css"; // Only needed if using built-in <Notification />
+import "notivue/animations.css"; // Only needed if using default animations
+import "notivue/notification-progress.css";
+
 import { createApp } from "vue";
+import { createNotivue } from "notivue";
+
 import App from "./App.vue";
 import router from "./router.ts";
 
+const notivue = createNotivue({
+  position: "top-right",
+  limit: 4,
+  enqueue: true,
+  avoidDuplicates: true,
+  notifications: {
+    global: {
+      duration: 10000
+    }
+  }
+});
 const app = createApp(App);
 
 app
   .use(router)
+  .use(notivue)
   .mount("#app");
