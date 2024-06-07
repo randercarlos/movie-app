19c804c0c42e2a26d4b52af20217859012104e96
Rander <rfreitas@uplexis.com.br>
2024-06-07 10:37:15 -0300

    style: remove comments and adjust code

--- src/pages/Movie.vue
+++ src/pages/Movie.vue
@@ -13,17 +13,15 @@ import { useMovieDetails } from "@/composables/useMovieDetails";
 import type { MovieDetails, MovieDetailsResponse } from "@/typings/interfaces";
 import { useMovieDetailsModelView } from "@/composables/useMovieDetailsModelView";
 import { handleError } from "@/utils/handleError";
-// import { resolvedPromises } from "@/utils/helper";
 
 const route = useRoute();
 const movieId = ref<number>(+route.params.movieId);
 const movieDetails = ref<MovieDetails>();
 
-//  await resolvedPromises(10000);
 try {
   const { data: movieDetailsResponse } = await useMovieDetails(movieId);
   const { data: movieDetailsModelView } = useMovieDetailsModelView(
-  movieDetailsResponse as MaybeRef<MovieDetailsResponse>,
+    movieDetailsResponse as MaybeRef<MovieDetailsResponse>
   );
   movieDetails.value = movieDetailsModelView.value;
 } catch(err: unknown) {
