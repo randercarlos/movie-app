b26e7b60b001dc410dd290002d80d5d593ea5e13
Rander <rfreitas@uplexis.com.br>
2024-06-08 19:37:56 -0300

    test: update test to new tv show routes

--- tests/unit/components/template/TheNavigation.spec.ts
+++ tests/unit/components/template/TheNavigation.spec.ts
@@ -20,14 +20,21 @@ describe("TheNavigation.vue", () => {
     expect(menuLinks[0].props().to).toBe("/");
     expect(menuLinks[0].html()).toContain("svg");
 
-    expect(menuLinks[1].props().to).toBe("/");
+    expect(menuLinks[1].props().to).toEqual({
+      name: "movies"
+    });
     expect(menuLinks[1].text()).toContain("Movies");
 
-    expect(menuLinks[2].props().to).toBe("/");
+
+    expect(menuLinks[2].props().to).toEqual({
+      name: "tvShows"
+    });
     expect(menuLinks[2].text()).toContain("TV Shows");
 
-    expect(menuLinks[3].props().to).toBe("/");
-    expect(menuLinks[3].text()).toContain("Actors");
+    // expect(menuLinks[3].props().to).toEqual({
+    //   name: "tvShows"
+    // });
+    // expect(menuLinks[3].text()).toContain("TV Shows");
 
     expect(avatarImg.exists()).toBe(true);
     expect(wrapper.findComponent({ name: "HeaderDropdown" }).exists()).toBe(true);
