0c4482e4b233eea7f174fd1cf34d4fb68c5a9cda
Rander <rfreitas@uplexis.com.br>
2024-06-08 19:34:24 -0300

    feat: create tv show skeletons

---
+++ src/components/skeleton/TvShowsCardSkeleton.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="mt-8">
+    <SkeletonLoader class="w-full h-[412px]" />
+    <div class="mt-2">
+      <SkeletonLoader class="w-full h-6" />
+      <div class="mt-1 flex items-center text-sm text-gray-400">
+        <SkeletonLoader class="w-full h-9 mt-1" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import SkeletonLoader from "./SkeletonLoader.vue";
+</script>
