3614dcac2cb8af5fc023de522960eb96ff310ebf
Rander <rfreitas@uplexis.com.br>
2024-06-23 09:24:30 -0300

    feat: create HeaderThemeButton component to toggle light/dark mode

---
+++ src/components/inputs/HeaderThemeButton.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="md:mr-4 mt-3 md:mt-0 text-2xl flex flex-col md:flex-row items-center">
+    <!-- Sun in light mode, Moon in dark mode, from Carbon -->
+    <button
+      class="dark:i-carbon-sun i-carbon-moon"
+      :title="`${isDark ? 'Light' : 'Dark'} Mode`"
+      @click="toggleDark()"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useDark, useToggle } from "@vueuse/core";
+
+const isDark = useDark({
+  selector: "html", //element to add attribute to
+  attribute: "class", // attribute name
+  valueDark: "dark", // attribute value for dark mode
+  valueLight: "light", // attribute value for light mode
+});
+const toggleDark = useToggle(isDark);
+</script>
