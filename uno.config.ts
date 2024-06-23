7bc808bd692dc5ef6d17a7399f34a28bfe49da82
Rander <rfreitas@uplexis.com.br>
2024-06-23 09:23:30 -0300

    chore: installs icons for dark/light mode

--- uno.config.ts
+++ uno.config.ts
@@ -1,9 +1,16 @@
 // uno.config.ts
-import { defineConfig, presetAttributify, presetUno } from "unocss";
+import { defineConfig, presetAttributify, presetUno, presetIcons } from "unocss";
 
 export default defineConfig({
   presets: [
     presetAttributify({ /* preset options */}),
     presetUno(),
+    presetIcons({
+      extraProperties: {
+      },
+      collections: {
+        carbon: () => import("@iconify-json/carbon/icons.json").then(i => i.default)
+      }
+    }),
   ],
 });
