19bdbf10cd96ae0872d4ed4cb464f08f600df011
Rander <rfreitas@uplexis.com.br>
2024-07-22 15:59:49 -0300

    test: improvements on useApiService and helper.spec.ts

--- tests/unit/composables/useApiService.spec.ts
+++ tests/unit/composables/useApiService.spec.ts
@@ -1,11 +1,66 @@
 import { useApiService } from "@/composables/useApiService";
 import { useFetch } from "@vueuse/core";
-import { describe, expect, it } from "vitest";
+import { describe, expect, it, vi } from "vitest";
+import { changeI18nGlobalLocale } from "../setupTests";
+import { I18nGlobalLocales } from "@/typings/enums";
+import nock from "nock";
+import CONFIG from "@/config";
+import { configurationTranslationsMock } from "#/mockData";
+import * as handleErrorFile from "@/utils/handleError";
+import * as helperFile from "@/utils/helper";
 
 describe("useApiService.ts", () => {
   it("create apiService with the correct configuration", async() => {
     const { apiService } = useApiService();
 
     expect(apiService).toBeTypeOf(typeof useFetch);
+  });
+
+  it("add a querystring to URL to get response using pt-BR locale", async() => {
+    nock(CONFIG.API_BASE_URL)
+      .persist()
+      .get("/configuration/primary_translations")
+      .query({ language: "pt-BR"})
+      .reply(200, configurationTranslationsMock);
+      
+    const logSpy = vi.spyOn(helperFile, "log");
+    const addQueryStringToURLSpy = vi.spyOn(helperFile, "addQueryStringToURL");
+      
+    // change global locale before load component
+    changeI18nGlobalLocale(I18nGlobalLocales.ptBR);
+    const { apiService } = useApiService();
+    const { data } = await apiService("/configuration/primary_translations")
+      .get()
+      .json();
+
+    expect(data).not.toBeNull();  
+    expect(logSpy).toHaveBeenCalled();
+    expect(addQueryStringToURLSpy).toHaveBeenCalled();
+  });
+
+  it("create a new error with status message and add in context if requests fails", async() => {
+    nock(CONFIG.API_BASE_URL)
+      .persist()
+      .get("/url_does_not_exist")
+      .reply(404, {
+        "success": false,
+        "status_code": 34,
+        "status_message": "The resource you requested could not be found."
+      });
+    
+    const ErrorSpy = vi.spyOn(global, "Error");
+    const handleErrorSpy = vi.spyOn(handleErrorFile, "handleError");
+
+    // change global locale before load component
+    changeI18nGlobalLocale(I18nGlobalLocales.enUS);
+
+    const { apiService } = useApiService();
+    const { error } = await apiService("/url_does_not_exist")
+      .get()
+      .json();
+
+    expect(error.value).not.toBeNull();
+    expect(ErrorSpy).toHaveBeenCalled();
+    expect(handleErrorSpy).toHaveBeenCalled();
   });
 });
--- tests/unit/utils/helper.spec.ts
+++ tests/unit/utils/helper.spec.ts
@@ -1,6 +1,16 @@
-import { describe, expect, it } from "vitest";
-import { formatDate, formatNumber, getEndpointWithQueryParams, resolvedPromises }
+import { describe, expect, it, vi } from "vitest";
+import { addQueryStringToURL, calculateAge,
+  formatDate,
+  formatNumber,
+  getEndpointWithQueryParams,
+  getYearFromDate,
+  isValidDate,
+  resolvedPromises,
+  truncateString}
   from "@/utils/helper";
+import type { Moment } from "moment";
+import moment from "moment";
+import { log } from "@/utils/helper";
 
 describe("helper.ts", () => {
   describe("getEndpointWithQueryParams()", () => {
@@ -74,18 +84,53 @@ describe("helper.ts", () => {
   });
 
   describe("formatDate()", () => {
-    it("format date with default format", () => {
-      const date = "2024-06-01";
-      const formattedDate = formatDate(date);
+    it("format date with default date format", () => {
+      const mapInputToResult: Record<string, Moment | string | null | undefined>[] = [
+        {
+          "dateInput": moment("2024-06-01", "YYYY-MM-DD"),
+          "dateResult": "06/01/2024"
+        },
+        {
+          "dateInput": "2024-06-01",
+          "dateResult": "06/01/2024"
+        },
+        {
+          "dateInput": null,
+          "dateResult": null
+        },
+        {
+          "dateInput": undefined,
+          "dateResult": null
+        }
+      ];
 
-      expect(formattedDate).toBe("06/01/2024");
+      mapInputToResult.forEach((inputToResult) =>
+        expect(formatDate(inputToResult.dateInput)).toBe(inputToResult.dateResult));
     });
 
-    it("format date with custom format", () => {
-      const date = "2024-06-01";
-      const formattedDate = formatDate(date, "DD/MM/YYYY");
+    it("format date with custom date format", () => {
+      const mapInputToResult: Record<string, Moment | string | null | undefined>[] = [
+        {
+          "dateInput": moment("2024-06-01", "YYYY-MM-DD"),
+          "dateResult": "01/06/2024"
+        },
+        {
+          "dateInput": "2024-06-01",
+          "dateResult": "01/06/2024"
+        },
+        {
+          "dateInput": null,
+          "dateResult": null
+        },
+        {
+          "dateInput": undefined,
+          "dateResult": null
+        }
+      ];
 
-      expect(formattedDate).toBe("01/06/2024");
+      const dateFormat = "DD/MM/YYYY";
+      mapInputToResult.forEach((inputToResult) =>
+        expect(formatDate(inputToResult.dateInput, dateFormat)).toBe(inputToResult.dateResult));
     });
   });
 
@@ -104,7 +149,7 @@ describe("helper.ts", () => {
     });
 
     it("resolved promises in custom time", async() => {
-      const waitFor = 500;
+      const waitFor = 1000;
       const start = Date.now();
 
       await resolvedPromises(waitFor);
@@ -117,4 +162,234 @@ describe("helper.ts", () => {
       expect(elapsed).toBeLessThan(waitFor + 100);
     });
   });
+
+  describe("getYearFromDate()", () => {
+    it("get year from date with default date format", () => {
+      const mapInputToResult: Record<string, Moment | string | null | undefined>[] = [
+        {
+          "dateInput": moment("2024-06-01", "YYYY-MM-DD"),
+          "dateResult": "2024"
+        },
+        {
+          "dateInput": "2024-06-01",
+          "dateResult": "2024"
+        },
+        {
+          "dateInput": null,
+          "dateResult": null
+        },
+        {
+          "dateInput": undefined,
+          "dateResult": null
+        }
+      ];
+
+      mapInputToResult.forEach((inputToResult) =>
+        expect(getYearFromDate(inputToResult.dateInput)).toBe(inputToResult.dateResult));
+    });
+
+    it("get year from date with custom date format", () => {
+      const mapInputToResult: Record<string, Moment | string | null | undefined>[] = [
+        {
+          "dateInput": moment("01/06/2024", "DD/MM/YYYY"),
+          "dateResult": "2024"
+        },
+        {
+          "dateInput": "01/06/2024",
+          "dateResult": "2024"
+        },
+        {
+          "dateInput": null,
+          "dateResult": null
+        },
+        {
+          "dateInput": undefined,
+          "dateResult": null
+        }
+      ];
+
+      const dateFormat = "DD/MM/YYYY";
+      mapInputToResult.forEach((inputToResult) =>
+        expect(getYearFromDate(inputToResult.dateInput, dateFormat))
+          .toBe(inputToResult.dateResult));
+    });
+  });
+
+  describe("calculateAge()", () => {
+    it("tries calculate age from date with default date format", () => {
+      const mapInputToResult = [
+        {
+          "dateInput": moment("2000-01-01", "YYYY-MM-DD"),
+          "dateResult": moment().diff(moment("2000-01-01", "YYYY-MM-DD"), "years")
+        },
+        {
+          "dateInput": "2000-01-01",
+          "dateResult": moment().diff(moment("2000-01-01", "YYYY-MM-DD"), "years")
+        },
+        {
+          "dateInput": null,
+          "dateResult": null
+        },
+        {
+          "dateInput": undefined,
+          "dateResult": null
+        }
+      ];
+
+      mapInputToResult.forEach((inputToResult) =>
+        expect(calculateAge(inputToResult.dateInput)).toBe(inputToResult.dateResult));
+    });
+
+    it("tries calculate age from date with custom date format", () => {
+      const dateFormat = "DD/MM/YYYY";
+      const mapInputToResult = [
+        {
+          "dateInput": moment("01/01/2000", dateFormat),
+          "dateResult": moment().diff(moment("01/01/2000", dateFormat), "years")
+        },
+        {
+          "dateInput": "01/01/2000",
+          "dateResult": moment().diff(moment("01/01/2000", dateFormat), "years")
+        },
+        {
+          "dateInput": null,
+          "dateResult": null
+        },
+        {
+          "dateInput": undefined,
+          "dateResult": null
+        }
+      ];
+
+      mapInputToResult.forEach(inputToResult => {
+        expect(calculateAge(inputToResult.dateInput, dateFormat)).toBe(inputToResult.dateResult);
+      });
+    });
+  });
+
+  describe("isValidDate()", () => {
+    it("checks if date input is a valid date with default date format", () => {
+      const mapInputToResult = [
+        {
+          "dateInput": moment("2024-07-10", "YYYY-MM-DD"),
+          "dateResult": true
+        },
+        {
+          "dateInput": "2024-07-10",
+          "dateResult": true
+        },
+        {
+          "dateInput": moment("2024-15-40", "YYYY-MM-DD"),
+          "dateResult": false
+        },
+        {
+          "dateInput": "2024-15-40",
+          "dateResult": false
+        },
+        {
+          "dateInput": null,
+          "dateResult": false
+        },
+        {
+          "dateInput": undefined,
+          "dateResult": false
+        }
+      ];
+
+      mapInputToResult.forEach((inputToResult) =>
+        expect(isValidDate(inputToResult.dateInput)).toBe(inputToResult.dateResult));
+    });
+
+    it("checks if date input is a valid date with custom date format", () => {
+      const dateFormat = "DD/MM/YYYY";
+      const mapInputToResult = [
+        {
+          "dateInput": moment("10/07/2024", dateFormat),
+          "dateResult": true
+        },
+        {
+          "dateInput": "10/07/2024",
+          "dateResult": true
+        },
+        {
+          "dateInput": moment("40/15/2024", dateFormat),
+          "dateResult": false
+        },
+        {
+          "dateInput": "40/15/2024",
+          "dateResult": false
+        },
+        {
+          "dateInput": null,
+          "dateResult": false
+        },
+        {
+          "dateInput": undefined,
+          "dateResult": false
+        }
+      ];
+
+      mapInputToResult.forEach(inputToResult => {
+        expect(isValidDate(inputToResult.dateInput, dateFormat)).toBe(inputToResult.dateResult);
+      });
+    });
+  });
+
+  describe("truncateString()", () => {
+    it("returns the original string if its length is less than or equals the max. number of chars.",
+      () => {
+        const string = "Isso é uma string de teste";
+
+        expect(truncateString(string, 30)).toBe(string);
+        expect(truncateString(string, 26)).toBe(string);
+      });
+
+    it("returns the original string if max. number of chars is less than or equals zero", () => {
+      const string = "Isso é uma string de teste";
+
+      expect(truncateString(string, -2)).toBe(string);
+    });
+
+    it("returns the truncate string if its length is greather than the max. number of chars. "
+      + "with default limit string", () => {
+      const string = "Isso é uma string de teste";
+
+      expect(truncateString(string, 20)).toBe("Isso é uma string de...");
+      expect(truncateString(string, 1)).toBe("I...");
+    });
+
+    it("returns the truncate string if its length is greather than the max. number of chars. "
+      + "with custom limit string", () => {
+      const string = "Isso é uma string de teste";
+      const limitString = "___";
+
+      expect(truncateString(string, 20, limitString)).toBe("Isso é uma string de___");
+      expect(truncateString(string, 1, limitString)).toBe("I___");
+    });
+  });
+
+  describe("addQueryStringToURL()", () => {
+    it("adds a query string to a URL",
+      () => {
+        const URL = "http://localhost:3000/api/v1/clients";
+        const queryStringKey = "approved";
+        const queryStringValue = true;
+
+        expect(addQueryStringToURL(URL, queryStringKey, String(queryStringValue)))
+          .toBe("http://localhost:3000/api/v1/clients?approved=true");
+      });
+  });
+
+  describe("log()", () => {
+    it("shows data in console.log()", () => {
+      const consoleLogSpy = vi.spyOn(console, "log");
+
+      log("Test message");
+
+      expect(consoleLogSpy).toHaveBeenCalledTimes(3);
+
+      consoleLogSpy.mockRestore();
+    });
+  });
+
 });
