0fca9b915d7285aa0a2515f42c283756ae309f98
Rander <rfreitas@uplexis.com.br>
2024-07-22 15:56:48 -0300

    test: improve tests for Footer and TheNavigation components

--- tests/unit/components/template/TheFooter.spec.ts
+++ tests/unit/components/template/TheFooter.spec.ts
@@ -1,21 +1,42 @@
-
-/* eslint-disable vue/one-component-per-file */
 import { describe, expect, it } from "vitest";
-import { mount } from "@vue/test-utils";
+import { mount, shallowMount } from "@vue/test-utils";
 import TheFooter from "@/components/template/TheFooter.vue";
+import enUS from "@/i18n/locales/en-US.json";
+import ptBR from "@/i18n/locales/pt-BR.json";
+import { I18nGlobalLocales } from "@/typings/enums";
+import { changeI18nGlobalLocale } from "#/unit/setupTests";
 
 describe("TheFooter.vue", () => {
-  it("renders correctly", () => {
-    const wrapper = mount(TheFooter);
+  it("renders correctly", async() => {
+    const wrapper = shallowMount(TheFooter);
 
-    expect(wrapper.html()).toContain("Powered by");
+    expect(wrapper.exists()).toBe(true);
   });
 
-  it("has a link to TMDb API documentation", () => {
+  it("renders link to TMDb API documentation", async() => {
     const wrapper = mount(TheFooter);
+
     const link = wrapper.find("a");
 
     expect(link.exists()).toBe(true);
     expect(link.attributes("href")).toBe("https://www.themoviedb.org/documentation/api");
+  });
+
+  it("show texts in en-US locale(default)", () => {
+    // change global locale before load component
+    changeI18nGlobalLocale(I18nGlobalLocales.enUS);
+
+    const wrapper = mount(TheFooter);
+
+    expect(wrapper.text()).toContain(enUS.footer.poweredBy);
+  });
+
+  it("show texts in pt-BR locale", () => {
+    // change global locale before load component
+    changeI18nGlobalLocale(I18nGlobalLocales.ptBR);
+
+    const wrapper = mount(TheFooter);
+
+    expect(wrapper.text()).toContain(ptBR.footer.poweredBy);
   });
 });
--- tests/unit/components/template/TheNavigation.spec.ts
+++ tests/unit/components/template/TheNavigation.spec.ts
@@ -1,8 +1,10 @@
-
-/* eslint-disable vue/one-component-per-file */
 import { describe, expect, it } from "vitest";
 import { mount, RouterLinkStub } from "@vue/test-utils";
 import TheNavigation from "@/components/template/TheNavigation.vue";
+import enUS from "@/i18n/locales/en-US.json";
+import ptBR from "@/i18n/locales/pt-BR.json";
+import { I18nGlobalLocales  } from "@/typings/enums";
+import { changeI18nGlobalLocale } from "#/unit/setupTests";
 
 describe("TheNavigation.vue", () => {
   it("renders correctly", () => {
@@ -10,33 +12,131 @@ describe("TheNavigation.vue", () => {
       global: {
         stubs: {
           RouterLink: RouterLinkStub,
-          HeaderDropdown: true
+          HeaderDropdown: true,
+          HeaderI18nButton: true,
+          HeaderThemeButtom: true
+        }
+      }
+    });
+
+    const headerDropdown = wrapper.findComponent({ name: "HeaderDropdown" });
+    const headerI18nButton = wrapper.findComponent({ name: "HeaderI18nButton" });
+    const headerThemeButtom = wrapper.findComponent({ name: "HeaderThemeButtom" });
+
+    expect(headerDropdown.exists()).toBe(true);
+    expect(headerI18nButton.exists()).toBe(true);
+    expect(headerThemeButtom.exists()).toBe(true);
+  });
+
+  it("renders logo correctly", () => {
+    const wrapper = mount(TheNavigation, {
+      global: {
+        stubs: {
+          RouterLink: RouterLinkStub,
+          HeaderDropdown: true,
+          HeaderI18nButton: true,
+          HeaderThemeButtom: true
         }
       }
     });
     const menuLinks = wrapper.findAllComponents(RouterLinkStub);
-    const avatarImg = wrapper.find("[data-test='avatar-img']");
 
     expect(menuLinks[0].props().to).toBe("/");
     expect(menuLinks[0].html()).toContain("svg");
+  });
 
-    expect(menuLinks[1].props().to).toEqual({
-      name: "movies"
+  it("renders avatar correctly", () => {
+    const wrapper = mount(TheNavigation, {
+      global: {
+        stubs: {
+          RouterLink: RouterLinkStub,
+          HeaderDropdown: true,
+          HeaderI18nButton: true,
+          HeaderThemeButtom: true,
+        }
+      }
     });
-    expect(menuLinks[1].text()).toContain("Movies");
-
-
-    expect(menuLinks[2].props().to).toEqual({
-      name: "tvShows"
-    });
-    expect(menuLinks[2].text()).toContain("TV Shows");
-
-    // expect(menuLinks[3].props().to).toEqual({
-    //   name: "tvShows"
-    // });
-    // expect(menuLinks[3].text()).toContain("TV Shows");
+    const avatarImg = wrapper.find("[data-test='avatar-img']");
 
     expect(avatarImg.exists()).toBe(true);
-    expect(wrapper.findComponent({ name: "HeaderDropdown" }).exists()).toBe(true);
+  });
+
+  it("renders dropdown search correctly", () => {
+    const wrapper = mount(TheNavigation, {
+      global: {
+        stubs: {
+          RouterLink: RouterLinkStub,
+          HeaderDropdown: true,
+          HeaderI18nButton: true,
+          HeaderThemeButtom: true
+        }
+      }
+    });
+    const headerDropdown = wrapper.findComponent({ name: "HeaderDropdown" });
+
+    expect(headerDropdown.exists()).toBe(true);
+  });
+
+
+  it("renders menu items correctly", () => {
+    const wrapper = mount(TheNavigation, {
+      global: {
+        stubs: {
+          RouterLink: RouterLinkStub,
+          HeaderDropdown: true,
+          HeaderI18nButton: true,
+          HeaderThemeButtom: true
+        }
+      }
+    });
+    const menuLinks = wrapper.findAllComponents(RouterLinkStub);
+
+    expect(menuLinks[0].props().to).toBe("/");
+    expect(menuLinks[0].html()).toContain("svg");
+    expect(menuLinks[1].props().to).toEqual({ name: "movies" });
+    expect(menuLinks[2].props().to).toEqual({ name: "tvShows" });
+    expect(menuLinks[3].props().to).toEqual({ name: "actors" });
+  });
+
+  it("show texts in en-US locale(default)", () => {
+    // change global locale before load component
+    changeI18nGlobalLocale(I18nGlobalLocales.enUS);
+
+    const wrapper = mount(TheNavigation, {
+      global: {
+        stubs: {
+          RouterLink: RouterLinkStub,
+          HeaderDropdown: true,
+          HeaderI18nButton: true,
+          HeaderThemeButtom: true
+        }
+      }
+    });
+    const menuLinks = wrapper.findAllComponents(RouterLinkStub);
+
+    expect(menuLinks[1].text()).toContain(enUS.header.menu.movies);
+    expect(menuLinks[2].text()).toContain(enUS.header.menu.tvShows);
+    expect(menuLinks[3].text()).toContain(enUS.header.menu.actors);
+  });
+
+  it("show texts in pt-BR locale", () => {
+    // change global locale before load component
+    changeI18nGlobalLocale(I18nGlobalLocales.ptBR);
+
+    const wrapper = mount(TheNavigation, {
+      global: {
+        stubs: {
+          RouterLink: RouterLinkStub,
+          HeaderDropdown: true,
+          HeaderI18nButton: true,
+          HeaderThemeButtom: true
+        }
+      }
+    });
+    const menuLinks = wrapper.findAllComponents(RouterLinkStub);
+
+    expect(menuLinks[1].text()).toContain(ptBR.header.menu.movies);
+    expect(menuLinks[2].text()).toContain(ptBR.header.menu.tvShows);
+    expect(menuLinks[3].text()).toContain(ptBR.header.menu.actors);
   });
 });
