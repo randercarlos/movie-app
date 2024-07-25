fbb32b5e2a42c86114e85477e59fa8a59a4fa7ad
Rander <rfreitas@uplexis.com.br>
2024-07-25 16:05:32 -0300

    chore: add created by with i18n  in footer. Add i18n title

--- src/components/template/TheFooter.vue
+++ src/components/template/TheFooter.vue
@@ -1,11 +1,20 @@
 <template>
   <footer class="border-t border-gray-800 light:border-gray-300">
-    <div class="container mx-auto text-sm px-4 py-6">
-      {{ t('footer.poweredBy') }} <a
-        href="https://www.themoviedb.org/documentation/api"
-        class="underline hover:text-gray-300"
-        target="_blank"
-      >TMDb API</a>
+    <div class="container mx-auto text-sm px-4 py-6 flex justify-between items-center">
+      <span>
+        {{ t('footer.poweredBy') }} <a
+          href="https://www.themoviedb.org/documentation/api"
+          class="underline hover:text-gray-300"
+          target="_blank"
+        >TMDb API</a>
+      </span>
+      <span>
+        {{ t('footer.createdBy') }} <a
+          href="https://www.linkedin.com/in/rander-carlos-308a63a8/"
+          class="underline hover:text-gray-300"
+          target="_blank"
+        >Rander Carlos</a>
+      </span>
     </div>
   </footer>
 </template>
